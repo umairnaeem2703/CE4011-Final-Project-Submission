262109616f333fd1abf56f1c75582d52f135689b
Umair Naeem <naeem.umair27@gmail.com>
2026-06-18 02:19:42 +0300

    settlement case fixed
    fixed member end results and visualization. 100% agreement with sap2000

--- src/post_processor.py
+++ src/post_processor.py
@@ -14,6 +14,7 @@ class PostProcessor:
         
         self.displacements = {}  # node_id -> [ux, uy, rz]
         self.member_forces = {}  # el_id -> list of local forces
+        self.member_end_forces = {}  # el_id -> {"i": [Fx, Fy, Mz], "j": [Fx, Fy, Mz]}
         self.nvm_data = {}       # el_id -> {"x": [...], "N": [...], "V": [...], "M": [...]}
         self.reactions = {}      # node_id -> [Fx, Fy, Mz]
         
@@ -137,8 +138,18 @@ class PostProcessor:
                     [N2], [V2], [M2]
                 ]
 
-            # 6. Transform raw local forces back to global for reaction calculation
+            # 6. Transform raw local forces back to global for reported end actions and reactions.
             f_global = math_utils.matmul(math_utils.transpose(T), f_local)
+            if el.type == 'truss':
+                self.member_end_forces[el_id] = {
+                    "i": [f_global[0][0], f_global[1][0], 0.0],
+                    "j": [f_global[2][0], f_global[3][0], 0.0],
+                }
+            else:
+                self.member_end_forces[el_id] = {
+                    "i": [f_global[0][0], f_global[1][0], f_global[2][0]],
+                    "j": [f_global[3][0], f_global[4][0], f_global[5][0]],
+                }
             
             if el.node_i.id in self.reactions:
                 self.reactions[el.node_i.id][0] += f_global[0][0]
@@ -240,6 +251,7 @@ class PostProcessor:
             nvm_data=self.nvm_data,
             dof_map=dof_map,
             load_case_id=load_case_id,
+            member_end_forces=self.member_end_forces,
         )
 
     def write_results(self, filepath: str):
@@ -277,33 +289,18 @@ class PostProcessor:
             f.write("\n")
             
             # --- MEMBER FORCES ---
-            f.write("2. MEMBER LOCAL END FORCES\n")
+            f.write("2. MEMBER END FORCES (GLOBAL JOINT ACTIONS)\n")
             f.write("-" * 80 + "\n")
-            f.write("Element  Node  Axial (kN)       Shear (kN)       Moment (kN-m)\n")
+            f.write("Element  Node  Fx (kN)          Fy (kN)          Mz (kN-m)\n")
             f.write("-" * 80 + "\n")
-            for el_id in sorted(self.member_forces.keys()):
+            for el_id in sorted(self.member_end_forces.keys()):
                 el = self.model.elements[el_id]
-                forces = self.member_forces[el_id]
-                
-                if el.type == 'truss':
-                    # Truss stored as: [N1, V1, N2, V2]
-                    f_i_axial  = forces[0][0]  # N1
-                    f_i_shear  = forces[1][0]  # V1 (always 0 for truss)
-                    f_i_moment = 0.0
-                    f_j_axial  = forces[2][0]  # N2
-                    f_j_shear  = forces[3][0]  # V2 (always 0 for truss)
-                    f_j_moment = 0.0
-                else:
-                    # Frame stored as: [N1, V1, M1, N2, V2, M2]
-                    f_i_axial  = forces[0][0]  # N1
-                    f_i_shear  = forces[1][0]  # V1
-                    f_i_moment = forces[2][0]  # M1
-                    f_j_axial  = forces[3][0]  # N2
-                    f_j_shear  = forces[4][0]  # V2
-                    f_j_moment = forces[5][0]  # M2
-                
-                f.write(f"{el_id:<8} {el.node_i.id:<4} {f_i_axial:<16.4f} {f_i_shear:<16.4f} {f_i_moment:<16.4f}\n")
-                f.write(f"{'':<8} {el.node_j.id:<4} {f_j_axial:<16.4f} {f_j_shear:<16.4f} {f_j_moment:<16.4f}\n")
+                forces = self.member_end_forces[el_id]
+                f_i = forces["i"]
+                f_j = forces["j"]
+
+                f.write(f"{el_id:<8} {el.node_i.id:<4} {f_i[0]:<16.4f} {f_i[1]:<16.4f} {f_i[2]:<16.4f}\n")
+                f.write(f"{'':<8} {el.node_j.id:<4} {f_j[0]:<16.4f} {f_j[1]:<16.4f} {f_j[2]:<16.4f}\n")
                 f.write("-" * 80 + "\n")
             f.write("\n")
             
